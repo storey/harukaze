9a3aa07f28bc49a9da258db3fa7b74901bdc9164
Grant Storey <grantjstorey@gmail.com>
2014-11-26 12:23:09 -0500

    Added Start Screen, Main Menu where user can move menu item selectors, though no menu items do anything.

---
+++ Assets/Scripts/StartScreenScript.js
@@ -0,0 +1,67 @@
+﻿#pragma strict
+
+// controls the main starting screen and shifts it to the side when the user hits a key
+
+// how long should it take for the screen to slide off?
+var slideTime : float = 0.5;
+
+// direction to slide;
+var slideLeft : boolean = true;
+
+// how far to move the screen each step
+private var stepDistance : float;
+
+// how many fixedUpdate steps it takes to fully remove the starting slide
+private var numSteps : int;
+
+// is the main menu showing? or is it not showing/sliding off
+private var showing : boolean = true;
+
+// this screen's transform
+private var myTransform : Transform;
+
+// holds the script for the main menu
+private var menuScript : menuSelectionScript;
+
+function Start () 
+{
+    // get the number of steps
+    numSteps = Mathf.Floor(slideTime/Time.fixedDeltaTime);
+    
+    // calculate the distance the slide must travel and how far to travel each step
+    var startSprite : Sprite = GetComponent(SpriteRenderer).sprite;
+    // get it fully off the screen, with a little extra fudge
+    var distance = startSprite.bounds.max.x - startSprite.bounds.min.x + 10.0;
+    if (slideLeft)
+    {
+        distance *= -1.0;
+    }
+    stepDistance = distance / numSteps;
+    
+    
+    myTransform = GetComponent(Transform);
+    
+    menuScript = GameObject.Find("MainMenu").GetComponent(menuSelectionScript);
+
+}
+
+function Update () 
+{
+    // check if the user presses a key, and if so start sliding the main menu off 
+    if(showing && Input.anyKeyDown)
+    {
+        showing = false;
+        menuScript.setActive(true);
+        StartCoroutine(slideScreen());
+    }
+}
+
+// rotate the circle the proper amount each fixed update
+function slideScreen()
+{
+    for (var i : int = 0; i < numSteps; i++)
+    {
+        yield WaitForFixedUpdate;
+        myTransform.position.x += stepDistance;
+    }
+}
---
+++ Assets/Scripts/menuSelectionScript.js
@@ -0,0 +1,56 @@
+﻿#pragma strict
+
+// controls selecting items from a menu by moving selectors.
+
+// name of selectors
+var selectorName : String = "MainMenuSelectors";
+
+// starting y position
+var startY : float = 0.883477;
+
+// distance between menu items
+var yDistance : float = 0.5937717;
+
+// number of menu items
+var numItems : int = 6;
+
+// the currently selected menu item
+private var currentSelection : int = 0;
+
+// transform of the selectors
+private var selectorTransform : Transform;
+
+// can the user use this menu?
+private var isActive : boolean = false;
+
+function Start() 
+{
+    selectorTransform = GameObject.Find(selectorName).GetComponent(Transform);
+    selectorTransform.position.y = startY;
+}
+
+// check whether the user is moving through the menu
+function Update() 
+{
+    if (isActive)
+    {
+        if (Input.GetKeyDown(KeyCode.W) && currentSelection != 0)
+        {
+            Debug.Log("up");
+            selectorTransform.position.y += yDistance;
+            currentSelection--;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && currentSelection != (numItems-1))
+        {
+            Debug.Log("down");
+            selectorTransform.position.y -= yDistance;
+            currentSelection++;
+        }
+    }
+}
+
+// set whether this menu is active to a
+function setActive(a : boolean)
+{
+    isActive = a;
+}
